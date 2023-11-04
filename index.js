d43d310c78cc23d4c7c80f13255b9e3ceb427968
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2023-11-04 10:35:35 +0800

    added x-axis

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
   const svgCanvass = {
-    width: 600,
+    width: 900,
     height: 500,
     padding: 40,
   };
 
   const createCanvass = () => {
-    return d3.select("body").append("svg").attr("width", svgCanvass.width);
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", svgCanvass.width)
+      .attr("height", svgCanvass.height);
+    return svg;
   };
-  const createTitle = () => {};
-  const createBar = () => {};
+
+  const createTitle = (svg) => {
+    return d3
+      .select("svg")
+      .append("text")
+      .attr("id", "title")
+      .attr("x", 350)
+      .attr("y", 40)
+      .text("USA Gross Domestic Product");
+  };
+  const createBar = (svg, dataset) => {
+    return svg
+      .selectAll("rect")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("width", svgCanvass.width + svgCanvass.padding / dataset.length)
+      .attr("height", (d) => d[1])
+      .attr("x", (d, i) => i * 3)
+      .attr("y", (d) => svgCanvass.height - (d[1] - svgCanvass.padding));
+  };
+
+  const createAxes = (svg, xScale) => {
+    return svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(0,${svgCanvass.height - svgCanvass.padding})`
+      )
+      .call(d3.axisBottom(xScale).tickFormat((d) => d3.timeFormat("%Y")(d)));
+  };
+
   const createTooltip = () => {};
 
   fetch(
@@ -17,12 +52,33 @@ document.addEventListener("DOMContentLoaded", () => {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      //   console.log(data); --------------> response
       const dataset = data?.data;
-      console.log(dataset);
-      createCanvass();
+      console.log(dataset); //----------------> dataset
+
+      //max and min
+      const dates = dataset.map((d) => d[0]);
+      console.log("dates:", dates);
+      const gdp = dataset.map((d) => d[1]);
+      const oldestDate = d3.min(dates);
+      console.log("oldestDate", oldestDate);
+      console.log("typeof oldestDate", typeof oldestDate);
+
+      const latestDate = d3.max(dates);
+      console.log("latestDate:", latestDate);
+
+      const maxGPD = d3.max(gdp);
+
+      //Scaling
+      const xScale = d3
+        .scaleLinear()
+        .domain([new Date(oldestDate), new Date(latestDate)])
+        .range([svgCanvass.padding, svgCanvass.width - svgCanvass.padding]);
+
+      const svg = createCanvass();
       createTitle();
-      createBar();
+      //   createBar(svg, dataset);
+      createAxes(svg, xScale);
       createTooltip();
     })
     .catch((err) => console.log(err));
